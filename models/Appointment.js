4b2b9c4a72cb0e2e15fe53da21674f26b29832e3
Varsha Kundarapu <123001134+Varsha2635@users.noreply.github.com>
2025-07-21 20:16:08 +0530

    Update Appointment.js

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,10 +1,1 @@
-const mongoose = require("mongoose");
 
-const appointmentSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
-  doctorId: { type: String, required: true }, // or you can use ObjectId if you store doctor profiles
-  datetime: { type: Date, required: true },
-  status: { type: String, enum: ["pending", "confirmed"], default: "pending" }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Appointment", appointmentSchema);
