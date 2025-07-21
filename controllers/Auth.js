b4ef02a2987b6601e85ad0453093f2e63efc3821
Varsha Kundarapu <123001134+Varsha2635@users.noreply.github.com>
2025-07-21 19:54:17 +0530

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -135,6 +135,8 @@ exports.login = async (req, res) => {
       const options = {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: true,
+        secure: true,       // Required for HTTPS (Render uses HTTPS)
+        sameSite: "None", 
       };
 
       res.cookie("token", token, options).status(200).json({
